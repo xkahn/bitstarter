b4d691db8b0696bbaff8b5df7acd67227bc8ce2d
Benjamin Kahn <xkahn@zoned.net>
2013-07-08 04:11:01 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require(’fs’);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var index = fs.readFileSync("index.html");
+  response.send(index.toString);
 });
 
 var port = process.env.PORT || 5000;
